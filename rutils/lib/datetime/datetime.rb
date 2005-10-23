2ca1ae50c14f207c4cb5b2dc93b750eaa64eb300
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-10-23 01:44:19 +0000

    Shifted flexion in distance_of_time_in_words to comply
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@48 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/datetime/datetime.rb
+++ rutils/lib/datetime/datetime.rb
@@ -18,19 +18,22 @@ module RuTils
 					return (distance_in_minutes==0) ? 'меньше минуты' : '1 минуту' unless include_seconds
 
 				case distance_in_seconds
-					 when 0..5	 then 'менее чем 5 секунд'
-					 when 6..10	 then 'менее чем 10 секунд'
-					 when 11..20 then 'менее чем 20 секунд'
+					 when 0..5	 then 'менее 5 секунд'
+					 when 6..10	 then 'менее 10 секунд'
+					 when 11..20 then 'менее 20 секунд'
 					 when 21..40 then 'пол-минуты'
 					 when 41..59 then 'меньше минуты'
 					 else					'1 минуту'
 				 end
 														 
-				 when 2..45			 then distance_in_minutes.propisju_items(2, "минута", "минуты", "минут") 
+				 when 2..45			 then distance_in_minutes.to_s + 
+				                      " " + distance_in_minutes.items("минута", "минуты", "минут") 
 				 when 46..90		 then 'около часа'
-				 when 90..1440	 then "около " + (distance_in_minutes.to_f / 60.0).round.items("час", "часа", "часов")
+				 when 90..1440	 then "около " + (distance_in_minutes.to_f / 60.0).round.to_s + 
+				                      " " + (distance_in_minutes.to_f / 60.0).round.items("часа", "часов", nil)
 				 when 1441..2880 then '1 день'
-				 else									(distance_in_minutes / 1440).round.propisju_items(1, "день", "дня", "дней")
+				 else									(distance_in_minutes / 1440).round.to_s + 
+				                      " " + (distance_in_minutes / 1440).round.items("день", "дня", "дней")
 			 end
 		end
 		
--- rutils/test/t_datetime.rb
+++ rutils/test/t_datetime.rb
@@ -5,6 +5,7 @@ require File.dirname(__FILE__) + '/../lib/rutils'
 class DistanceOfTimeTest < Test::Unit::TestCase
 	def test_distance_of_time_in_words
 		assert_equal "меньше минуты", RuTils::DateTime::distance_of_time_in_words(0, 50)
-		assert_equal "две минуты", RuTils::DateTime::distance_of_time_in_words(0, 140)
+		assert_equal "2 минуты", RuTils::DateTime::distance_of_time_in_words(0, 140)
+		assert_equal "около 2 часов", RuTils::DateTime::distance_of_time_in_words(0, 60*114)
 	end
 end
