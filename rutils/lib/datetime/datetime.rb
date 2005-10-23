9a6ea36771ccaaed87068111ca97946a30703dca
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-10-23 01:45:24 +0000

    Shifted flexion in distance_of_time_in_words to comply
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@49 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/datetime/datetime.rb
+++ rutils/lib/datetime/datetime.rb
@@ -29,6 +29,7 @@ module RuTils
 				 when 2..45			 then distance_in_minutes.to_s + 
 				                      " " + distance_in_minutes.items("минута", "минуты", "минут") 
 				 when 46..90		 then 'около часа'
+				 # исключение, сдвигаем на один влево чтобы соответствовать падежу
 				 when 90..1440	 then "около " + (distance_in_minutes.to_f / 60.0).round.to_s + 
 				                      " " + (distance_in_minutes.to_f / 60.0).round.items("часа", "часов", nil)
 				 when 1441..2880 then '1 день'
