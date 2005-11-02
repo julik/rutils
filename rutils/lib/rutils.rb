f9ca6ed986fcb1c2356400df41cf7d3a6ee9543f
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-11-02 12:48:34 +0000

    Added countries
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@70 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/rutils.rb
+++ rutils/lib/rutils.rb
@@ -12,11 +12,21 @@ module RuTils
 
   # Версия RuTils
 	VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
+	
+	def self.load_component(name) #:nodoc:
+    require RuTils::INSTALLATION_DIRECTORY + "/lib/#{name}/#{name}"
+	end
+
+	def self.reload_component(name) #:nodoc:
+    load RuTils::INSTALLATION_DIRECTORY + "/lib/#{name}/#{name}.rb"
+	end
 end
 
-require File.dirname(__FILE__) + '/pluralizer/pluralizer'
-require File.dirname(__FILE__) + '/gilenson/gilenson_port' #Предельно идентичный порт Тыпографицы
-#require File.dirname(__FILE__) + '/gilenson/gilenson' #Гиленсон, рефакторенный нами - todo
-require File.dirname(__FILE__) + '/datetime/datetime' # Дата и время без локалей
-require File.dirname(__FILE__) + '/transliteration/transliteration' # Транслит
-require File.dirname(__FILE__) + '/integration/integration' # Интеграция с rails, textile и тд
+
+RuTils::load_component :pluralizer #Выбор числительного и сумма прописью
+require File.dirname(__FILE__) + '/gilenson/gilenson_port'
+# RuTils::load_component :gilenson
+RuTils::load_component :datetime # Дата и время без локалей
+RuTils::load_component :transliteration # Транслит
+RuTils::load_component :integration # Интеграция с rails, textile и тд
+RuTils::load_component :countries # Данные о странах на русском и английском
