3ea659f2e3ca4da1efc8f996b117c4c052a4b2a2
mash <mash@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-11-05 11:52:25 +0000

    test for Time#strftime_ru
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@73 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/test/t_datetime.rb
+++ rutils/test/t_datetime.rb
@@ -10,3 +10,20 @@ class DistanceOfTimeTest < Test::Unit::TestCase
 		assert_equal "около 3 часов", RuTils::DateTime::distance_of_time_in_words(0, 60*120+60*60)
 	end
 end
+
+class StrftimeRuTest < Test::Unit::TestCase
+	def test_strftime_ru
+		assert_equal "Суббота, тридцатьпервое декабря 2005 года", Time.local(2005,"dec",31).strftime_ru("%A, %E %Y года")
+		assert_equal "Среда, тридцатое ноября 2005 года", Time.local(2005,"nov",30).strftime_ru("%A, %E %Y года")
+		assert_equal "Вторник, двадцатьдевятое ноября 2005 года", Time.local(2005,"nov",29).strftime_ru("%A, %E %Y года")
+		assert_equal "Понедельник, двадцатьпервое ноября 2005 года", Time.local(2005,11,21).strftime_ru("%A, %E %Y года")
+		assert_equal "Воскресенье, двадцатое ноября 2005 года", Time.local(2005,11,20).strftime_ru("%A, %E %Y года")
+		assert_equal "Суббота, девятнадцатое ноября 2005 года", Time.local(2005,11,19).strftime_ru("%A, %E %Y года")
+		assert_equal "Пятница, одиннадцатое ноября 2005 года", Time.local(2005,11,11).strftime_ru("%A, %E %Y года")
+		assert_equal "Вторник, первое ноября 2005 года", Time.local(2005,11,1).strftime_ru("%A, %E %Y года")
+		assert_equal "Воскресенье, первое января 2006 года", Time.local(2006,1,1).strftime_ru("%A, %E %Y года")
+		assert_equal "Вск Воскресенье Янв Январь первое января 1", Time.local(2006,1,1).strftime_ru("%a %A %b %B %E %e")
+		assert_equal "Сб Суббота Дек Декабрь тридцатьпервое декабря 31", Time.local(2005,12,31).strftime_ru("%a %A %b %B %E %e")
+		assert_equal "31.12.2005", Time.local(2005,12,31).strftime_ru
+	end
+end
