da309116581c444d50f5a9a46b0c3b952f2f5ec8
mash <mash@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-11-05 12:01:08 +0000

    Time#strftime_ru
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@74 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/datetime/datetime.rb
+++ rutils/lib/datetime/datetime.rb
@@ -1,13 +1,17 @@
-require 'date'
+#~ Убираем?
+#~ require 'date'
 
 module RuTils
 	module DateTime
-	  ABBR_MONTHNAMES = [nil] + %w{ Янв Фев Мар Апр Май Июн Июл Авг Сен Окт Ноя Дек }
-	  MONTHNAMES = [nil] + %w{ Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь }
-	  INFLECTED_MONTHNAMES = [nil] + %w{ Января Февраля Марта Апреля Мая Июня Июля Авгуса Сентября Октября Ноября Декабря }
-	  DAYNAMES = %w(Воскресенье Понедельник Вторник Среда Четверг Пятница Суббота)
-	  ABBR_DAYNAMES = %w(Вск Пн Вт Ср Чт Пт Сб)
-	  
+		ABBR_MONTHNAMES = [nil] + %w{ Янв Фев Мар Апр Май Июн Июл Авг Сен Окт Ноя Дек }
+		MONTHNAMES = [nil] + %w{ Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь }
+		INFLECTED_MONTHNAMES = [nil] + %w{ Января Февраля Марта Апреля Мая Июня Июля Авгуса Сентября Октября Ноября Декабря }
+		DAYNAMES = %w(Воскресенье Понедельник Вторник Среда Четверг Пятница Суббота)
+		ABBR_DAYNAMES = %w(Вск Пн Вт Ср Чт Пт Сб)
+		
+		MONTHNAMES_E = [nil] + %w{января февраля марта апреля мая июня июля августа сентября октября ноября декабря}
+		DAYNAMES_E = [nil] + %w{первое второе третье четвёртое пятое шестое седьмое восьмое девятое десятое одиннадцатое двенадцатое тринадцатое четырнадцатое пятнадцатое шестнадцатое семнадцатое восемнадцатое девятнадцатое двадцатое двадцать тридцатое тридцатьпервое}
+		
 		def self.distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, absolute = false) #nodoc
 			from_time = from_time.to_time if from_time.respond_to?(:to_time)
 			to_time = to_time.to_time if to_time.respond_to?(:to_time)
@@ -42,8 +46,31 @@ module RuTils
 	end
 end
 
-class Date
-	def ru_strftime(*args)
-		RuTils::DateTime::RussianDate.new(self).strftime(*args)
+#~ Убираем?
+#~ class Date
+	#~ def ru_strftime(*args)
+		#~ RuTils::DateTime::RussianDate.new(self).strftime(*args)
+	#~ end
+#~ end
+
+class Time
+	def strftime_ru(date='%d.%m.%Y', r=RuTils::DateTime)
+		# %e, %E отсутствуют в оригинале:
+		#   %e - day of the month as a decimal number, a single digit is preceded by a space (range ' 1' to '31')
+		#   %E - day of the month like "первое января"
+		case self.day
+			when 1..20		then day = r::DAYNAMES_E[self.day]
+			when 21..29	then day = r::DAYNAMES_E[21] + r::DAYNAMES_E[self.day-20]
+			when 30			then day = r::DAYNAMES_E[22]
+			when 31			then day = r::DAYNAMES_E[23]
+		end
+		date.gsub!(/%E/, day + " " + r::MONTHNAMES_E[self.mon])
+		date.gsub!(/%e/, self.day.to_s)
+		
+		date.gsub!(/%a/, r::ABBR_DAYNAMES[self.wday])
+		date.gsub!(/%A/, r::DAYNAMES[self.wday])
+		date.gsub!(/%b/, r::ABBR_MONTHNAMES[self.mon])
+		date.gsub!(/%B/, r::MONTHNAMES[self.mon])
+		self.strftime(date)
 	end
 end
