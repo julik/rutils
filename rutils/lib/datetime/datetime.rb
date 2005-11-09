2fc346f78507e5345c5c897a0c1508af2242ed8f
mash <mash@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-11-09 11:26:28 +0000

    RuTils::DateTime::ru_strftime -- alias_method for Time#strftime
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@77 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/datetime/datetime.rb
+++ rutils/lib/datetime/datetime.rb
@@ -1,27 +1,16 @@
-#~ Убираем?
-#~ require 'date'
-
 module RuTils
 	module DateTime
-		ABBR_MONTHNAMES = [nil] + %w{ Янв Фев Мар Апр Май Июн Июл Авг Сен Окт Ноя Дек }
-		MONTHNAMES = [nil] + %w{ Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь }
-		INFLECTED_MONTHNAMES = [nil] + %w{ Января Февраля Марта Апреля Мая Июня Июля Авгуса Сентября Октября Ноября Декабря }
-		DAYNAMES = %w(Воскресенье Понедельник Вторник Среда Четверг Пятница Суббота)
-		ABBR_DAYNAMES = %w(Вск Пн Вт Ср Чт Пт Сб)
-		
-		MONTHNAMES_E = [nil] + %w{января февраля марта апреля мая июня июля августа сентября октября ноября декабря}
-		DAYNAMES_E = [nil] + %w{первое второе третье четвёртое пятое шестое седьмое восьмое девятое десятое одиннадцатое двенадцатое тринадцатое четырнадцатое пятнадцатое шестнадцатое семнадцатое восемнадцатое девятнадцатое двадцатое двадцать тридцатое тридцатьпервое}
-		
+	
 		def self.distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, absolute = false) #nodoc
 			from_time = from_time.to_time if from_time.respond_to?(:to_time)
 			to_time = to_time.to_time if to_time.respond_to?(:to_time)
 			distance_in_minutes = (((to_time - from_time).abs)/60).round
 			distance_in_seconds = ((to_time - from_time).abs).round
-
+			
 			case distance_in_minutes
 				when 0..1
 					return (distance_in_minutes==0) ? 'меньше минуты' : '1 минуту' unless include_seconds
-
+				
 				case distance_in_seconds
 					 when 0..5	 then 'менее 5 секунд'
 					 when 6..10	 then 'менее 10 секунд'
@@ -30,7 +19,7 @@ module RuTils
 					 when 41..59 then 'меньше минуты'
 					 else					'1 минуту'
 				 end
-														 
+				
 				 when 2..45			 then distance_in_minutes.to_s + 
 				                      " " + distance_in_minutes.items("минута", "минуты", "минут") 
 				 when 46..90		 then 'около часа'
@@ -43,34 +32,34 @@ module RuTils
 			 end
 		end
 		
+		@@ignored = "\xFF\xFF\xFF\xFF"# %% == Literal "%" character 
+		
+		def self.ru_strftime(date='%d.%m.%Y', time='')
+			date.gsub!(/%%/, @@ignored)
+			date.gsub!(/%a/, Date::RU_ABBR_DAYNAMES[time.wday])
+			date.gsub!(/%A/, Date::RU_DAYNAMES[time.wday])
+			date.gsub!(/%b/, Date::RU_ABBR_MONTHNAMES[time.mon])
+			date.gsub!(/%B/, Date::RU_MONTHNAMES[time.mon])
+			date.gsub!(@@ignored, '%%')
+		end
 	end
 end
 
-#~ Убираем?
-#~ class Date
-	#~ def ru_strftime(*args)
-		#~ RuTils::DateTime::RussianDate.new(self).strftime(*args)
-	#~ end
-#~ end
+class Date
+	RU_MONTHNAMES = [nil] + %w{ Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь }
+	RU_DAYNAMES = %w(Воскресенье Понедельник Вторник Среда Четверг Пятница Суббота)
+	RU_ABBR_MONTHNAMES = [nil] + %w{ Янв Фев Мар Апр Май Июн Июл Авг Сен Окт Ноя Дек }
+	RU_ABBR_DAYNAMES = %w(Вск Пн Вт Ср Чт Пт Сб)
+	RU_INFLECTED_MONTHNAMES = [nil] + %w{ Января Февраля Марта Апреля Мая Июня Июля Авгуса Сентября Октября Ноября Декабря }
+	# RU_DAYNAMES_E -- оставить?
+	RU_DAYNAMES_E = [nil] + %w{первое второе третье четвёртое пятое шестое седьмое восьмое девятое десятое одиннадцатое двенадцатое тринадцатое четырнадцатое пятнадцатое шестнадцатое семнадцатое восемнадцатое девятнадцатое двадцатое двадцать тридцатое тридцатьпервое}
+end
 
 class Time
-	def strftime_ru(date='%d.%m.%Y', r=RuTils::DateTime)
-		# %e, %E отсутствуют в оригинале:
-		#   %e - day of the month as a decimal number, a single digit is preceded by a space (range ' 1' to '31')
-		#   %E - day of the month like "первое января"
-		case self.day
-			when 1..20		then day = r::DAYNAMES_E[self.day]
-			when 21..29	then day = r::DAYNAMES_E[21] + r::DAYNAMES_E[self.day-20]
-			when 30			then day = r::DAYNAMES_E[22]
-			when 31			then day = r::DAYNAMES_E[23]
-		end
-		date.gsub!(/%E/, day + " " + r::MONTHNAMES_E[self.mon])
-		date.gsub!(/%e/, self.day.to_s)
-		
-		date.gsub!(/%a/, r::ABBR_DAYNAMES[self.wday])
-		date.gsub!(/%A/, r::DAYNAMES[self.wday])
-		date.gsub!(/%b/, r::ABBR_MONTHNAMES[self.mon])
-		date.gsub!(/%B/, r::MONTHNAMES[self.mon])
-		self.strftime(date)
+	alias_method :strftime_norutils, :strftime
+	
+	def strftime(date)
+		RuTils::DateTime::ru_strftime(date, self) if RuTils::overrides_enabled?
+		strftime_norutils(date)
 	end
 end
