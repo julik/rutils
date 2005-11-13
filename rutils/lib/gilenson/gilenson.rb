37c3d675ad10cd98f374824375fa27615c30dd9a
mash <mash@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2005-11-13 23:43:39 +0000

    removed replace apmersand in tags (& => &#38;)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@138 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/gilenson/gilenson.rb
+++ rutils/lib/gilenson/gilenson.rb
@@ -433,10 +433,11 @@ class RuTils::Gilenson::Formatter
       
       def reinsert_fragments(text, fragments)
         fragments.each { |fragment|
-          fragment.gsub!(/&(?!#0*38;)/, self.glyph[:amp]) if !@settings['raw_output']
+          #~ пока непонятно
+          #~ fragment.gsub!(/&(?!(#0*38|(amp);)/, self.glyph[:amp]) if (!@settings['raw_output'] || !@settings["html"])
           fragment.gsub!(/ (title|alt)=((?:(\')([^\']*)(\'))|(?:(\")([^\"]*)(\")))/uim) {
             " #{$1}=#{$3}" + self.process($4.to_s) + "#{$5}#{$6}" + self.process($7.to_s) + "#{$8}"
-          } if !@settings['skip_attr']
+          } unless @settings['skip_attr']
           text.sub!(@mark_tag, fragment)
         }
       end
