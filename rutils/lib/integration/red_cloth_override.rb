07eaab3fb1e29d1ecc1e8464a1aad36dc08f874c
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2007-02-17 15:31:35 +0000

    Fix redcloth integration
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@204 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- rutils/lib/integration/red_cloth_override.rb
+++ rutils/lib/integration/red_cloth_override.rb
@@ -12,7 +12,16 @@ if defined?(Object::RedCloth) and (!RedCloth.instance_methods.include?(:stock_pg
     # Вместо того чтобы влезать в чужие таблицы мы просто заменим Textile Glyphs на Gilenson - и все будут рады.  
     alias_method  :stock_pgl, :pgl
     def pgl(text) #:nodoc:
-      RuTils::overrides_enabled? ?  text.replace(RuTils::Gilenson::Formatter.new(text).to_html) :  stock_pgl(text)
+      if RuTils::overrides_enabled?
+        # Suspend the spaces in the start and end of the block because Gilenson chews them off, and RedCloth feeds them to us in packs
+        # that appear between tag starts/stops. We mess up the formatting but preserve the spacing.
+        spaces =  Array.new(2,'')
+        text.gsub!(/\A([\s]+)/) { spaces[0] = $1; '' }
+        text.gsub!(/(\s+)\Z/) { spaces[1] = $1; '' }
+        text.replace(spaces[0].to_s + RuTils::Gilenson::Formatter.new(text).to_html + spaces[1].to_s)
+      else
+        stock_pgl(text)
+      end
     end
   end 
 end
