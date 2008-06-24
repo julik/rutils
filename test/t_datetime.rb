13756e9e8c1c7a41f34e661313f1803117b7e6d1
yaroslav <yaroslav@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2008-06-24 18:49:29 +0000

    strftime with %d now takes exactly 2 characters -- as intented [#17657]
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@248 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- test/t_datetime.rb
+++ test/t_datetime.rb
@@ -22,6 +22,8 @@ class StrftimeRuTest < Test::Unit::TestCase
       assert_equal "%сб, %суббота, %дек, %декабрь", Time.local(2005,"dec",31).strftime("%%%a, %%%A, %%%b, %%%B")
       assert_equal "Сегодня: 31 декабря, суббота, 2005 года", Time.local(2005,"dec",31).strftime("Сегодня: %d %B, %A, %Y года")
       assert_equal "Сегодня: ноябрь, 30 число, дождик в четверг, а год у нас - 2006", Time.local(2006,11,30).strftime("Сегодня: %B, %d число, дождик в %A, а год у нас - %Y")
+      assert_equal "01 декабря", Time.local(1985, "dec", 01).strftime("%d %B")
+      assert_equal "11 декабря", Time.local(1985, "dec", 11).strftime("%d %B")
       
       date = Date.new(2005, 12, 31)
       assert_equal "дек декабрь сб суббота", "#{Date::RU_ABBR_MONTHNAMES[date.mon]} #{Date::RU_MONTHNAMES[date.mon]} #{Date::RU_ABBR_DAYNAMES[date.wday]} #{Date::RU_DAYNAMES[date.wday]}"
@@ -33,6 +35,8 @@ class StrftimeRuTest < Test::Unit::TestCase
       assert_equal "%a, %A, %b, %B", Time.local(2005,"dec",31).strftime("%%a, %%A, %%b, %%B")
       assert_equal "%Sat, %Saturday, %Dec, %December", Time.local(2005,"dec",31).strftime("%%%a, %%%A, %%%b, %%%B")
       assert_equal "Сегодня: 31 December, Saturday, 2005 год", Time.local(2005,"dec",31).strftime("Сегодня: %d %B, %A, %Y год")
+      assert_equal "01 December", Time.local(1985, "dec", 01).strftime("%d %B")
+      assert_equal "11 December", Time.local(1985, "dec", 11).strftime("%d %B")
       
       date = Date.new(2005, 11, 9)
       assert_equal "Nov November Wed Wednesday", "#{Date::ABBR_MONTHNAMES[date.mon]} #{Date::MONTHNAMES[date.mon]} #{Date::ABBR_DAYNAMES[date.wday]} #{Date::DAYNAMES[date.wday]}"
