eb5fb912289d74e88eadcfbaab5a82525806389c
yaroslav <yaroslav@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2008-07-18 06:13:17 +0000

    cosmetics
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@280 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- lib/datetime/datetime.rb
+++ lib/datetime/datetime.rb
@@ -3,7 +3,7 @@ module RuTils
   
     def self.distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, absolute = false) #nodoc
       from_time = from_time.to_time if from_time.respond_to?(:to_time)
-      to_time = to_time.to_time if to_time.respond_to?(:to_time)
+      to_time   = to_time.to_time if to_time.respond_to?(:to_time)
       distance_in_minutes = (((to_time - from_time).abs)/60).round
       distance_in_seconds = ((to_time - from_time).abs).round
       
@@ -13,14 +13,14 @@ module RuTils
         
         case distance_in_seconds
            when 0..5   then 'менее 5 секунд'
-           when 6..10   then 'менее 10 секунд'
+           when 6..10  then 'менее 10 секунд'
            when 11..20 then 'менее 20 секунд'
            when 21..40 then 'пол-минуты'
            when 41..59 then 'меньше минуты'
-           else          '1 минуту'
+           else             '1 минуту'
          end
         
-         when 2..45       then distance_in_minutes.to_s + 
+         when 2..45      then distance_in_minutes.to_s + 
                               " " + distance_in_minutes.items("минута", "минуты", "минут") 
          when 46..90     then 'около часа'
          # исключение, сдвигаем на один влево чтобы соответствовать падежу
@@ -53,7 +53,6 @@ class Date
   RU_ABBR_MONTHNAMES = [nil] + %w{ янв фев мар апр май июн июл авг сен окт ноя дек }
   RU_ABBR_DAYNAMES = %w(вск пн вт ср чт пт сб)
   RU_INFLECTED_MONTHNAMES = [nil] + %w{ января февраля марта апреля мая июня июля августа сентября октября ноября декабря }
-  # RU_DAYNAMES_E -- оставить?
   RU_DAYNAMES_E = [nil] + %w{первое второе третье четвёртое пятое шестое седьмое восьмое девятое десятое одиннадцатое двенадцатое тринадцатое четырнадцатое пятнадцатое шестнадцатое семнадцатое восемнадцатое девятнадцатое двадцатое двадцать тридцатое тридцатьпервое}
 end
 
@@ -66,7 +65,6 @@ class Time
 end
 
 class Date
-
   # Inside rails we have date formatting
   if self.instance_methods.include?('strftime')
     alias_method :strftime_norutils, :strftime
