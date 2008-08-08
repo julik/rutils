6a0b6a4d57ab2857509cd375cf0f0a9303a1ac15
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2008-08-08 10:15:44 +0000

    Fix the doc
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@289 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 =begin
   Этот файл - заглушка для Rails-плагина. 
   Если поместить rutils/ в папку vendor/plugins/ вашего Rails-приложения, 
-  RuTils будет автоматически загружен с включенным флагом <tt>RuTils::overrides = true</tt>
+  RuTils будет автоматически загружен с включенным флагом <tt>RuTils::overrides = true</tt>.
+  В ActionController::Base будет установлен пре-фильтр устанавливающий флаг <tt>overrides.</tt>
 =end
 require File.join(File.dirname(__FILE__), 'lib', 'rutils')
 RuTils::overrides = true
