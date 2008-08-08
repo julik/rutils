14252de1b3102409f43238b9fabd821b9b826897
julik <julik@a3ef6f13-15a0-41b7-8655-729d286dc6c2>
2008-08-08 10:09:04 +0000

    Fix outstanding issues
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rutils/trunk@287 a3ef6f13-15a0-41b7-8655-729d286dc6c2

--- test/test_rails_filter.rb
+++ test/test_rails_filter.rb
@@ -1,9 +1,10 @@
-ActionController::Routing::Routes.draw {  |map| map.connect ':controller/:action/:id' }
 require 'action_controller/test_process'
-
+load File.dirname(__FILE__) +  '/../lib/integration/init.rb'
+ActionController::Routing::Routes.draw {  |map| map.connect ':controller/:action/:id' }
 
 class RutiledController < ActionController::Base #:nodoc:
   def overridden
+    raise "Overrides are off" unless RuTils::overrides_enabled?
     render :inline => '<%= Time.local(2008,8,8).strftime("%B") %>'
   end
   def rescue_action(e); raise e; end
@@ -18,7 +19,7 @@ class RailsFilterTest < Test::Unit::TestCase
   end
   
   def test_overrides_preserved_in_render
-    get :overridden
+    assert_nothing_raised { get :overridden }
     assert_response :success
     assert_equal "Август", @response.body
   end
