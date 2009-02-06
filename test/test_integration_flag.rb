c8a2082b0a12a9fbab9f94ccc49729d7dbcdabff
Julik <me@julik.nl>
2009-02-06 01:34:28 +0100

    Убираем тесты хаков подальше

---
+++ test/test_integration_flag.rb
@@ -0,0 +1,17 @@
+$KCODE = 'u'
+require 'test/unit'
+
+require File.dirname(__FILE__) + '/../lib/rutils'
+
+class IntegrationFlagTest < Test::Unit::TestCase
+  def setup
+    RuTils::overrides = true
+  end
+  
+  def test_integration_flag
+    RuTils::overrides = false
+    assert !RuTils::overrides_enabled?
+    RuTils::overrides = true    
+    assert RuTils::overrides_enabled?
+  end  
+end
