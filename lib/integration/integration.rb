baf98578b06b2aefadbe14e03412582da0f90ac5
Julik <me@julik.nl>
2009-02-07 23:42:07 +0100

    Do not load missing overrides

--- lib/integration/integration.rb
+++ lib/integration/integration.rb
@@ -1,3 +1,1 @@
-require File.dirname(__FILE__) + '/blue_cloth_override' if defined?(BlueCloth)
-require File.dirname(__FILE__) + '/red_cloth_override' if defined?(RedCloth)
 require File.dirname(__FILE__) + '/rails_date_helper_override' if defined?(ActionView)
