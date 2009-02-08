a6ea69d128fbfb29a21a07e3999559919099b438
Julik <me@julik.nl>
2009-02-08 01:00:25 +0100

    Rename test

--- test/extras/integration_maruku.rb
+++ test/extras/integration_maruku.rb
@@ -6,7 +6,7 @@ require 'maruku'
 class MarukuIntegrationTest < Test::Unit::TestCase
   C = RuTils::Gilenson::MarukuExtra
   
-  def test_integration_markdown
+  def test_integration_maruku
     
     assert_equal "<p>И вот&#160;&#171;они пошли туда&#187;, и&#160;шли шли&#160;шли</p>", 
       C.new('И вот "они пошли туда", и шли шли шли').to_html
