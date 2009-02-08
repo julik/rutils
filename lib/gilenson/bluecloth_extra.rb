5092af40b761dc6d6c8d659e831e559fc3233ae3
Julik <me@julik.nl>
2009-02-08 01:03:36 +0100

    BlueCloth + Maruku

--- lib/gilenson/bluecloth_extra.rb
+++ lib/gilenson/bluecloth_extra.rb
@@ -2,10 +2,6 @@
 # BlueCloth с поддержкой Gilenson
 class RuTils::Gilenson::BlueClothExtra < BlueCloth
   def to_html(*opts)
-    if RuTils::overrides_enabled?
-      RuTils::Gilenson::Formatter.new(super(*opts)).to_html
-    else
-      super(*opts)
-    end
+    RuTils::Gilenson::Formatter.new(super(*opts)).to_html
   end
 end
--- test/extras/integration_bluecloth.rb
+++ test/extras/integration_bluecloth.rb
@@ -7,9 +7,7 @@ class BlueclothIntegrationTest < Test::Unit::TestCase
   C = RuTils::Gilenson::BlueClothExtra
   
   def test_integration_markdown
-    
     assert_equal "<p>И вот&#160;&#171;они пошли туда&#187;, и&#160;шли шли&#160;шли</p>", 
       C.new('И вот "они пошли туда", и шли шли шли').to_html
-    
   end
 end
