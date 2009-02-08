3ae35a9f90d5e16f642664aac1c6944684068367
Julik <me@julik.nl>
2009-02-08 01:05:46 +0100

    Бесшумный init.rb. 1.0.0.

--- init.rb
+++ init.rb
@@ -5,7 +5,8 @@
   RuTils будет автоматически загружен с включенным флагом <tt>RuTils::overrides = true</tt>.
   В ActionController::Base будет установлен пре-фильтр устанавливающий флаг <tt>overrides.</tt>
 =end
-require File.join(File.dirname(__FILE__), 'lib', 'rutils')
+require File.dirname(__FILE__) +  '/lib/rutils' unless defined?(RuTils)
+
 RuTils::overrides = true
 require File.dirname(__FILE__) + '/lib/integration/rails_pre_filter' 
 
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*- 
 module RuTils
   # Версия RuTils
-  VERSION = '0.3.0'
+  VERSION = '1.0.0'
 end
